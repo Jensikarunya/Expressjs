8c8b79cfa3b949dcc58e457b0f88a40f17775ebd
Jensikarunya <38131585+Jensikarunya@users.noreply.github.com>
2025-04-18 22:10:30 +0530

    Delete express-movie-app directory

--- express-movie-app/routes/movies.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { getMovies, getMovieById } = require('../controllers/movieController')
-const router = express.Router()
-
-router.get('/', getMovies) 
-router.get('/:id', getMovieById) 
-
-module.exports = router
