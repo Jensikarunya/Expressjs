19577c05e2e63d48813fb32d0af07d8e2be538ac
Jensikarunya <38131585+Jensikarunya@users.noreply.github.com>
2025-04-18 22:12:24 +0530

    Add files via upload

---
+++ express-movie-app/routes/movies.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const { getMovies, getMovieById } = require('../controllers/movieController')
+const router = express.Router()
+
+router.get('/', getMovies) 
+router.get('/:id', getMovieById) 
+
+module.exports = router
